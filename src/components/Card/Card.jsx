0a46c2c98add243d1ff29ea62903995038cd5b1c
Gabriel Matos <gsilva505@hotmail.com>
2022-09-24 14:35:16 -0300

    retornando herois via axios

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,11 +3,11 @@ import './card.css'
 import { Panel } from 'rsuite';
 import { BsFillCartPlusFill } from 'react-icons/bs'
 
-export default function Card({ addToCard, name }) {
+export default function Card({ addToCard, name, thumb }) {
     return (
-        <Panel shaded bordered bodyFill style={{ display: 'inline-block', width: 240 }}>
-            <img src="https://pbs.twimg.com/profile_images/1553942611030953986/3bduP-xR_400x400.jpg" height="240" />
-            <Panel header="Spider-Man">
+        <Panel shaded bordered bodyFill style={{ display: 'inline-block', width: 240, marginRight: 50, marginBottom: 25 }}>
+            <img src={thumb} height="240" />
+            <Panel header={name}>
                 <button onClick={addToCard}><BsFillCartPlusFill size='25px' /></button>
             </Panel>
         </Panel>
--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './content.css'
 import Card from '../Card/Card'
 import { Message, useToaster } from 'rsuite';
@@ -8,22 +8,23 @@ const md5 = 'f52b1ad718e7dcfb558e77325dce0c5f'
 const timestamp = '1663986710468'
 const apikey = 'a8c6429f75de8e7ee58493ef64bbd9d6'
 
-axios({
-    method: "get",
-    url: `http://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${apikey}&hash=${md5}&limit=100`,
-}).then(function (response) {
-    const data = response.data.data.results
-    // data.forEach(element => {
-    //     console.log(element.name);
-    // });
-});
-
 // console.log(Math.floor(Date.now()));
 
 export default function Content() {
     const type = 'success';
     const placement = 'topStart';
     const toaster = useToaster();
+
+    const [heros, setHeros] = useState([])
+
+    useEffect(() => {
+        axios.get(`http://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${apikey}&hash=${md5}&limit=100`)
+            .then((response) => {
+                setHeros(response.data.data.results)
+            })
+            .catch(err => console.log(err));
+    }, [])
+
 
     const message = (
         <Message showIcon type={type}>
@@ -37,7 +38,13 @@ export default function Content() {
 
     return (
         <div className="content">
-            <Card addToCard={addCart} />
+            {heros.map(hero => {
+                return (
+                    <div>
+                        <Card addToCard={addCart} name={hero.name} thumb={`${hero.thumbnail.path}.${hero.thumbnail.extension}`} />
+                    </div>
+                )
+            })}
         </div>
     )
 }
